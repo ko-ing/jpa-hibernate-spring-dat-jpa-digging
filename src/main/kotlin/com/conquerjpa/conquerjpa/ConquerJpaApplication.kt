ce117a8ccf99e86be0af97be2d88c64374dcd7c5
ko-ing <318921@naver.com>
2022-09-30 00:08:01 +0900

    first commit

---
+++ src/main/kotlin/com/conquerjpa/conquerjpa/ConquerJpaApplication.kt
@@ -0,0 +1,11 @@
+package com.conquerjpa.conquerjpa
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConquerJpaApplication
+
+fun main(args: Array<String>) {
+    runApplication<ConquerJpaApplication>(*args)
+}
---
+++ src/test/kotlin/com/conquerjpa/conquerjpa/ConquerJpaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.conquerjpa.conquerjpa
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ConquerJpaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
